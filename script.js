25eb61ff8b7e4cbb365d2c1a3355b48310f3bef3
BK-UP <141661501+BK-UP@users.noreply.github.com>
2023-09-06 18:22:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,8 +9,12 @@ $(document).ready(function () {
         let msg = $("#message").val();
         let check = msg.trim();
         let url = (youtube(msg));
+        let iframeCheck = "";
+        console.log(url);
+        if(url!=null)
+            iframeCheck = `<iframe src="https://www.youtube.com/embed/${url[1]}" ></iframe>`;
         if (check !== "") {
-            $(".messages").append(`<div class="alL alert alert-primary col-4 offset-4"><div>${msg}</div><iframe src="https://www.youtube.com/embed/${url[1]}" ></iframe></div>`);
+            $(".messages").append(`<div class="alL alert alert-primary col-4 offset-4"><div>${msg}</div>${iframeCheck}</div>`);
         }
         $("#message").val("");
     });
@@ -18,8 +22,12 @@ $(document).ready(function () {
         let msg = $("#message").val();
         let check = msg.trim();
         let url = (youtube(msg));
+        let iframeCheck = "";
+        console.log(url);
+        if(url!=null)
+            iframeCheck = `<iframe src="https://www.youtube.com/embed/${url[1]}" ></iframe>`;
         if (check !== "") {
-            $(".messages").append(`<div class="alL alert alert-primary col-4 offset-4"><div>${msg}</div><iframe src="https://www.youtube.com/embed/${url[1]}" ></iframe></div>`);
+            $(".messages").append(`<div class="alR alert alert-info col-4 offset-4"><div>${msg}</div>${iframeCheck}</div>`);
         }
         $("#message").val("");
     });
